66d9c6cc926ecc4d4d7dbfbcbee24b164c5de37e
ruangomes <ruangomes@bluelightcard.co.uk>
2024-05-27 14:18:32 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,56 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Form, Formik } from "formik";
 import "./App.css";
+import { Input } from "./components/Input";
+import { SuccessButton } from "./components/SuccessButton";
 import { Assistant } from "./assistant";
 
-// async function testCall() {
-//   try {
-//     const message = "Which is the capital of Albania?";
-//     const response = await openai.chat.completions.create({
-//       model: "gpt-4o",
-//       messages: [{ role: "user", content: message }],
-//       temperature: 0,
-//       max_tokens: 1000,
-//     });
-//     console.log(response, "response");
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
+async function App() {
+	// const [message, setMessage] = useState<string>("");
+	// const [assistantResponse, setAssistantResponse] = useState<string>("");
+	// useEffect(() => {
+	//   (async () => {
+	//     const assistant = new Assistant();
+	//     console.log(assistant, "assistant");
+	//     await assistant.Init();
+	//     await assistant.UserMessage(
+	//       "json how can I recycle https://franoidelivery.com.br/wp-content/uploads/2021/03/Pet-2-Litros-Coca-Cola-PNG.png"
+	//     );
+	//     await assistant.RunThread();
+	//     const res = await assistant.ResolveRun();
 
-function App() {
-  const [message, setMessage] = useState<string>("");
-  const [assistantResponse, setAssistantResponse] = useState<string>("");
-  useEffect(() => {
-    (async () => {
-      const assistant = new Assistant();
-      console.log(assistant, "assistant");
-      await assistant.Init();
-      await assistant.UserMessage(
-        "json how can I recycle https://franoidelivery.com.br/wp-content/uploads/2021/03/Pet-2-Litros-Coca-Cola-PNG.png"
-      );
-      await assistant.RunThread();
-      const res = await assistant.ResolveRun();
+	//     console.log(res, "res");
+	//     //if (res) setAssistantResponse(res.content[0]);
+	//   })().catch((err) => console.log(err));
+	// }, [message]);
 
-      console.log(res, "res");
-      //if (res) setAssistantResponse(res.content[0]);
-    })().catch((err) => console.log(err));
-  }, [message]);
+	const assistant = await new Assistant().Init();
+	async function onImageUpload() {
+		console.log(assistant)
+		console.log('heyo')
+	}
 
-  return (
-    <>
-      <div>
-        <form action="" method="post" encType="multipart/form-data">
-          <label htmlFor="image">Upload image:</label>
-          <input type="file" accept="image/*;capture=camera" />
-          <br />
-          <input type="submit" name="submit" value="Submit" />
-        </form>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="pt-[38px]">
+				<Formik
+					initialValues={{}}
+					onSubmit={onImageUpload}
+					// validationSchema={schema}
+				>
+					{() => (
+						<Form className="flex flex-col gap-3">
+							<Input />
+							<div className="flex pt-5">
+								<SuccessButton
+								// disabled={Object.keys(errors).length > 0}
+								/>
+							</div>
+						</Form>
+					)}
+				</Formik>
+			</div>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/Input.tsx
@@ -0,0 +1,7 @@
+export function Input() {
+	return (
+		<div>
+			<input type="file" className="file-input w-full max-w-xs" />
+		</div>
+	);
+}
